6a230e0ea143daf647dffced780c1c4e01ef6368
adosikas <adosikas@users.noreply.github.com>
2022-11-18 20:57:49 +0100

    Plot wall despawn

--- src/synth_mapping_helper/__init__.py
+++ src/synth_mapping_helper/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.4"
+__version__ = "0.9.5"
 
 from . import movement 
 from . import pattern_generation
--- src/synth_mapping_helper/finalize.py
+++ src/synth_mapping_helper/finalize.py
@@ -20,7 +20,10 @@ FINALIZED_BOOKMARK = {"time": 0.0, "name": "#smh_finalized"}
 BEATMAP_JSON_FILE = "beatmap.meta.bin"
 DIFFICULTIES = ("Easy", "Normal", "Hard", "Expert", "Master", "Custom")
 NOTE_COLORS = {"right": "red", "left": "cyan", "single": "lime", "both": "gold"}
-QUEST_WALL_DELAY = 0.051  # 51ms to guard against rounding errors 
+
+RENDER_WINDOW = 4  # 4 seconds of elements are rendered
+WALL_DESPAWN_PC = 80  # limit of visible walls, earlier ones despawn
+WALL_DESPAWN_QUEST = 40
 
 def get_parser():
     parser = ArgumentParser(
@@ -37,7 +40,8 @@ def get_parser():
     parser.add_argument("input", type=Path, help="Input file")
     parser.add_argument("output", type=Path, help="Output file")
     parser.add_argument("--revert", action="store_true", help="Reverse finalizing options")
-    parser.add_argument("--plot", action="store_true", help="Plot velocity and acceleration")
+    parser.add_argument("--plot-notes", action="store_true", help="Plot note velocity and acceleration")
+    parser.add_argument("--plot-walls", action="store_true", help="Plot walls for pc and quest")
     return parser
 
 def abort(reason: str):
@@ -56,6 +60,7 @@ def main(options):
                 outzip.writestr(info, inzip.read(info.filename))
         # load beatmap json
         beatmap = json.loads(inzip.read(BEATMAP_JSON_FILE))
+        bpm = beatmap["BPM"]
         finalized = FINALIZED_BOOKMARK in beatmap["Bookmarks"]["BookmarksList"]
         if not options.revert:
             if finalized:
@@ -72,15 +77,15 @@ def main(options):
                     w["position"][1] -= 2.1 * synth_format.GRID_SCALE
                 else:
                     w["position"][1] += 2.1 * synth_format.GRID_SCALE
-        if options.plot:
-            bpm = beatmap["BPM"]
-            for diff in DIFFICULTIES:
-                notes_dict = beatmap["Track"][diff]
-                if not notes_dict:
-                    continue
-                fig, axs = plt.subplots(5, 1, sharex=True, figsize=(16, 8))
-                fig.suptitle(f"{beatmap['Author']} - {beatmap['Name']}: {diff}")
-                (ax_x, ax_y, ax_vel, ax_acc, ax_walls) = axs
+
+        for diff in DIFFICULTIES:
+            # skip difficulties without notes
+            if not beatmap["Track"][diff]:
+                continue
+            if options.plot_notes:
+                fig, axs = plt.subplots(4, 1, sharex=True, figsize=(16, 8))
+                fig.suptitle(f"{beatmap['Author']} - {beatmap['Name']}: {diff} / Notes")
+                (ax_x, ax_y, ax_vel, ax_acc) = axs
                 ax_x.set_ylabel("X-Position (sq)")
                 ax_x.set_ylim((8, -8))
                 ax_x.grid(True)
@@ -93,53 +98,19 @@ def main(options):
                 ax_acc.set_ylabel("Acceleration (sq/s²)")
                 ax_acc.set_ylim((0, 50))
 
-                ax_walls.set_ylabel("Walls")
-                ax_walls.set_yticks([])
-
-                ax_walls.set_xlabel("time (s)")
-                ax_walls.xaxis.set_major_formatter(min_sec_formatter)
+                axs[-1].set_xlabel("time (s)")
+                axs[-1].xaxis.set_major_formatter(min_sec_formatter)
                 # bookmarks
                 for bookmark in beatmap["Bookmarks"]["BookmarksList"]:
                     time = int(bookmark["time"]) / 64 * 60 / bpm
                     for ax in axs:
                         ax.axvline(time, color="grey")
-                    ax_x.text(time, 0.99, bookmark["name"], ha='left', va='bottom', rotation=45, transform=ax_x.get_xaxis_transform())
-                # walls
-                walls: dict[str, list[float]] = {w_type: [] for w_type in synth_format.WALL_TYPES}
-                for slide in beatmap["Slides"][diff]:
-                    walls[synth_format.WALL_LOOKUP[slide["slideType"]]].append(slide["time"] / 64 * 60 / bpm)
-                walls["crouch"] = [w["time"] / 64 * 60 / bpm for w in beatmap["Crouchs"][diff]]
-                walls["square"] = [w["time"] / 64 * 60 / bpm for w in beatmap["Squares"][diff]]
-                walls["triangle"] = [w["time"] / 64 * 60 / bpm for w in beatmap["Triangles"][diff]]
-
-                wall_markers = {
-                    "wall_left": ("s", "left", 7),
-                    "wall_right": ("s", "right", 6),
-                    "angle_left": ("o", "left", 5),
-                    "angle_right":  ("o", "right", 4),
-                    "center": ("d", "full", 3),
-                    "crouch": ("s", "top", 2),
-                    "triangle": ("^", "none", 1),
-                    "square": ("s", "none", 0),
-                }
-                hidden: list[tuple[str, float]] = []
-                for wall_type, (marker, fill, y) in wall_markers.items():
-                    last_time = None
-                    for time in sorted(walls[wall_type]):
-                        quest_hidden = last_time is not None and time - last_time < QUEST_WALL_DELAY
-                        if quest_hidden:
-                            print(f"Wall hidden on Quest: {wall_type} @ {int(time/60)}:{time%60:06.3f}, distance {int((time - last_time) * 1000)} ms")
-                            hidden.append((wall_type, time))
-                        else:
-                            last_time = time
-                            ax_walls.plot([time], [y], marker=marker, fillstyle=fill, color="green")
-                # always draw hidden ones on top
-                for wall_type, time in hidden:
-                    marker, fill, y = wall_markers[wall_type]
-                    ax_walls.plot([time], [y], marker=marker, fillstyle=fill, color="red")
+                    axs[0].text(time, 0.99, bookmark["name"], ha='left', va='bottom', rotation=45, transform=axs[0].get_xaxis_transform())
                 
                 # notes & rails
                 positions = [{} for _ in synth_format.NOTE_TYPES]
+                notes_dict = beatmap["Track"][diff]
+
                 for time in sorted(notes_dict):
                     note_list = notes_dict[time]
                     for note in note_list:
@@ -159,6 +130,7 @@ def main(options):
                             # rail
                             ax_x.plot(pos[:, 2], pos[:, 0], color=color, marker="")
                             ax_y.plot(pos[:, 2], pos[:, 1], color=color, marker="")
+
                 for note_type, poss in enumerate(positions):
                     if len(poss) <= 1:
                         continue
@@ -182,7 +154,99 @@ def main(options):
 
                 fig.tight_layout()
                 plt.show()
-                # fig.savefig(f"{diff}.png")
+                # fig.savefig(f"{diff}_notes.png")
+
+            if options.plot_walls:
+                fig, axs = plt.subplots(3, 1, sharex=True, figsize=(16, 8))
+                fig.suptitle(f"{beatmap['Author']} - {beatmap['Name']}: {diff} / Walls")
+                (ax_density, ax_pc, ax_quest) = axs
+                ax_density.set_ylabel("Walls visible")
+                ax_density.set_ylim([0, 100])
+                ax_pc.set_ylabel("Walls Status (PC)")
+                ax_pc.set_yticks([])
+                ax_quest.set_ylabel("Wall Status (Quest)")
+                ax_quest.set_yticks([])
+
+                axs[-1].set_xlabel("time (s)")
+                axs[-1].xaxis.set_major_formatter(min_sec_formatter)
+                # bookmarks
+                for bookmark in beatmap["Bookmarks"]["BookmarksList"]:
+                    time = int(bookmark["time"]) / 64 * 60 / bpm
+                    for ax in axs:
+                        ax.axvline(time, color="grey")
+                    axs[0].text(time, 0.99, bookmark["name"], ha='left', va='bottom', rotation=45, transform=axs[0].get_xaxis_transform())
+
+                ax_density.axhline(40, color="lightblue")
+                ax_density.axhline(80, color="grey")
+
+                # walls
+                walls: dict[str, list[float]] = {w_type: [] for w_type in synth_format.WALL_TYPES}
+                for slide in beatmap["Slides"][diff]:
+                    walls[synth_format.WALL_LOOKUP[slide["slideType"]]].append(slide["time"] / 64 * 60 / bpm)
+                walls["crouch"] = [w["time"] / 64 * 60 / bpm for w in beatmap["Crouchs"][diff]]
+                walls["square"] = [w["time"] / 64 * 60 / bpm for w in beatmap["Squares"][diff]]
+                walls["triangle"] = [w["time"] / 64 * 60 / bpm for w in beatmap["Triangles"][diff]]
+
+                wall_markers = {
+                    "wall_left": ("s", "left", 7),
+                    "wall_right": ("s", "right", 6),
+                    "angle_left": ("o", "left", 5),
+                    "angle_right":  ("o", "right", 4),
+                    "center": ("d", "full", 3),
+                    "crouch": ("s", "top", 2),
+                    "triangle": ("^", "none", 1),
+                    "square": ("s", "none", 0),
+                }
+
+                walls_all: list[tuple[float, str]] = sorted({time: wall_type for wall_type in wall_markers for time in walls[wall_type]}.items())
+                visible: dict[float, int] = {}
+                for i, (time, _) in enumerate(walls_all):
+                    end = time + RENDER_WINDOW
+                    count = 1
+                    for other_time, _ in walls_all[i+1:]:
+                        if other_time > end:
+                            break
+                        count += 1
+                    visible[time] = count
+
+                sorted_visible = sorted(visible.items())
+                ax_density.plot([time for time, _ in sorted_visible], [count for _, count in sorted_visible])
+
+
+                quest_ok: dict[float, str] = {}
+                quest_hidden: dict[float, str] = {}
+
+                seconds_per_tick = 60 / (64 * bpm)  # [seconds / minute] / ([ticks / beat] * [beats / minute])
+                quest_wall_delay = np.ceil(0.05 / seconds_per_tick) * seconds_per_tick
+
+                for wall_type in wall_markers:
+                    last_time = None
+                    for time in sorted(walls[wall_type]):
+                        hidden_on_quest = last_time is not None and time - last_time < quest_wall_delay
+                        if hidden_on_quest:
+                            quest_hidden[time] = wall_type
+                        else:
+                            last_time = time
+                            quest_ok[time] = wall_type
+
+                # pc
+                for time, wall_type in walls_all:
+                    marker, fill, y = wall_markers[wall_type]
+                    ax_pc.plot([time], [y], marker=marker, fillstyle=fill, color="green" if visible[time] < WALL_DESPAWN_PC else "orange")
+    
+                # quest: draw ok ones first
+                for time, wall_type in sorted(quest_ok.items()):
+                    marker, fill, y = wall_markers[wall_type]
+                    ax_quest.plot([time], [y], marker=marker, fillstyle=fill, color="green" if visible[time] < WALL_DESPAWN_QUEST else "orange")
+                # quest: draw hidden ones "on top"
+                for time, wall_type in sorted(quest_hidden.items()):
+                    marker, fill, y = wall_markers[wall_type]
+                    ax_quest.plot([time], [y], marker=marker, fillstyle=fill, color="red")
+
+                fig.tight_layout()
+                plt.show()
+                # fig.savefig(f"{diff}_walls.png")
+
         if not options.revert:
             beatmap["Bookmarks"]["BookmarksList"].append(FINALIZED_BOOKMARK)
         # write modified beatmap json
