73bd5e682d0864e8e48d858ad291996c03455d4c
adosikas <adosikas@users.noreply.github.com>
2025-07-15 09:29:37 +0200

    Revert "Don't wait for pyinstaller update"
    This reverts commit 2ef6007166fdbf1939fc66a7342de759e181fd4d.

--- hook-plotly.py
+++
@@ -1,6 +0,0 @@
-# https://github.com/pyinstaller/pyinstaller-hooks-contrib/commit/c61421f026ef9f339c3d0f73cb655a6d8dc67b80
-from PyInstaller.utils.hooks import collect_data_files
-from PyInstaller.utils.hooks import collect_submodules
-
-datas = collect_data_files('plotly', includes=['package_data/**/*.*', 'validators/**/*.*'])
-hiddenimports = collect_submodules('plotly.validators') + ['pandas', 'cmath']
